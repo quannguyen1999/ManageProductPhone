b648d2dd896a6c3c88023db4cb19ef12d3f0868a
quannguyen1999 <nguyendanganhquan99@gmail.com>
2021-05-05 02:32:58 +1200

    add

--- src/main/java/com/main/controller/ProductController.java
+++ src/main/java/com/main/controller/ProductController.java
@@ -3,13 +3,25 @@ package com.main.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.main.model.Product;
 
 @Controller
 @RequestMapping(value = "product")
 public class ProductController {
+	
 	@RequestMapping(value = "add")
 	public String addProduct(Model model) {
-		model.addAttribute("content", "Product");
-		return "product";
+		model.addAttribute("product", new Product());
+		return "addProduct";
 	}
+	
+//	add-process
+	@RequestMapping(value = "add-process",method = RequestMethod.POST)
+	public String addProcessProduct(Product product) {
+		System.out.println(product);
+		return "redirect:/add";
+	}
+
 }
